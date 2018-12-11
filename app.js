36b20e96f0aedcc95f4fc01ab8c051a4b482096a
robertvenanzi <robertgvenanzi@gmail.com>
2018-12-10 20:27:27 -0800

    Structuring Modules

--- app.js
+++ app.js
@@ -1,17 +1,114 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
 
-    //some code
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            ID = 0;
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            data.allItems[type].push(newitem);
+            return newItem;
+        }
+    };
+
+
 })();
 
 // UI CONTROLLER
 var UIController = (function () {
-    // some code
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+
+    };
+
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
 
-    document.querySelector('add--btn');
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
+
+
+    var ctrlAddItem = function () {
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. add the item to the budget controller
+
+        // 3. Add the item to the Ui
+
+        // 4. Display the budget on the UI
+
+    }
+
+    return {
+        init: function () {
+            console.log('app started')
+            setupEventListeners();
+        }
+    }
+
 
 })(budgetController, UIController);
+
+controller.init();
