7a8434a1c26811a4daf73606605e252ca92997e7
robertvenanzi <robertgvenanzi@gmail.com>
2018-12-10 18:18:01 -0800

    InitialProject Setup / With Markup and Styles'

---
+++ app.js
@@ -0,0 +1,17 @@
+// BUDGET CONTROLLER
+var budgetController = (function () {
+
+    //some code
+})();
+
+// UI CONTROLLER
+var UIController = (function () {
+    // some code
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+
+    document.querySelector('add--btn');
+
+})(budgetController, UIController);
