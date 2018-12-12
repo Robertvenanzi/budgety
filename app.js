5068a8b08dac2cac6de362c23a1e6ef1c4797ac6
robertvenanzi <robertgvenanzi@gmail.com>
2018-12-11 21:00:08 -0800

    added if statement for ctrlAddItem to fix empty and NAN entries

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     var updateBudget = function () {
 
-        // 1. Calculate and update budget
+        // 1. Calculate the budget
 
         // 2. return the budget
 
@@ -167,15 +167,19 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 1. Get the field input data
         input = UICtrl.getInput();
 
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 4. Clear the fields
+            UICtrl.clearfields();
 
-        // 4. Clear the fields
-        UICtrl.clearfields();
+            // Calculate and update budget
+            updateBudget();
+        }
 
     };
 
